edd902527476df32842110bc8931e026ff4daec0
SchreiberDirk <60484604+SchreiberDirk@users.noreply.github.com>
2020-02-01 13:19:51 +0100

    Überarbeitet

--- AnyMotion/Program.cs
+++ AnyMotion/Program.cs
@@ -8,52 +8,62 @@ namespace AddOneProblem
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4 };
+            Ausgabe(array);
+
+            int[] array1 = { 1, 2, 3, 9 };
+            Ausgabe(array1);
+
+            int[] array2 = { 9 };
+            Ausgabe(array2);
+
+            int[] array3 = { 9, 9 };
+            Ausgabe(array3);
+
+            int[] array4 = { 9, 9, 9 };
+            Ausgabe(array4);
+
+            int[] array5 = { 9, 9, 9, 9, 9 };
+            Ausgabe(array5);
+
+        }
+        private static void Ausgabe(int[] array)
+        {
+            PrintArray(array);
+            Console.WriteLine();
+            AddOne(array);
+
+            if (array[0] == 0)
+            {
+                Array.Resize<int>(ref array, array.Length + 1);
+                array[0] = 1;
+            }
 
             PrintArray(array);
-            Console.WriteLine($"Länge: {array.Length}");
             Console.WriteLine();
-            int[] newArray = AddOne(array);
-            PrintArray(newArray);
-            Console.WriteLine($"Länge: {newArray.Length}");
-            Console.ReadKey();
-
+            Console.WriteLine("--------------");
+            Console.WriteLine();
         }
 
         private static void PrintArray(int[] array) => Console.WriteLine(string.Join(" ", array));
 
-
-        private static int[] AddOne(int[] array)
+        private static void AddOne(int[] array)
         {
+
             for (int i = array.Length - 1; i >= 0; i--)
             {
-                if (array[i] < 9)
+                if (array[i] == 9)
                 {
-                    array[i] = array[i] + 1;
-                    break;
+                    array[i] = 0;
                 }
                 else
                 {
-                    array[i] = 0;
-                    array[i - 1] = array[i - 1] + 1;
-                    if (array[i - 1] > 9)
-                    {
-                        array[i - 1] = 0;
-                        array[i - 2] = array[i - 2] + 1;
-                    }
+                    array[i] += 1;
                     break;
                 }
             }
-            if (array[0] > 9)
-            {
-                Array.Resize(ref array, array.Length + 1);
-                array[0] = 1;
-            }
-            return array;
-
         }
     }
 }
