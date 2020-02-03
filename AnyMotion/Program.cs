eca7a2120f314f67aa8507c075b2620915f71e92
SchreiberDirk <60484604+SchreiberDirk@users.noreply.github.com>
2020-02-03 11:44:05 +0100

    Program.cs
    - Ausgabemethode in Main überarbeitet
    - Resize des Arrays in AddOne verlagert

--- AnyMotion/Program.cs
+++ AnyMotion/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,37 +10,18 @@ namespace AddOneProblem
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4 };
-            Ausgabe(array);
-
-            int[] array1 = { 1, 2, 3, 9 };
-            Ausgabe(array1);
-
-            int[] array2 = { 9 };
-            Ausgabe(array2);
-
-            int[] array3 = { 9, 9 };
-            Ausgabe(array3);
-
-            int[] array4 = { 9, 9, 9 };
-            Ausgabe(array4);
-
-            int[] array5 = { 9, 9, 9, 9, 9 };
-            Ausgabe(array5);
-
+            Ausgabe(new int[] { 1, 2, 3, 4 });
+            Ausgabe(new int[] { 1, 2, 3, 9 });
+            Ausgabe(new int[] { 9 });
+            Ausgabe(new int[] { 9, 9 });
+            Ausgabe(new int[] { 9, 9, 9 });
+            Ausgabe(new int[] { 9, 9, 9, 9, 9 });
         }
         private static void Ausgabe(int[] array)
         {
             PrintArray(array);
             Console.WriteLine();
-            AddOne(array);
-
-            if (array[0] == 0)
-            {
-                Array.Resize<int>(ref array, array.Length + 1);
-                array[0] = 1;
-            }
-
+            AddOne(ref array);
             PrintArray(array);
             Console.WriteLine();
             Console.WriteLine("--------------");
@@ -49,9 +30,8 @@ namespace AddOneProblem
 
         private static void PrintArray(int[] array) => Console.WriteLine(string.Join(" ", array));
 
-        private static void AddOne(int[] array)
+        private static void AddOne(ref int[] array)
         {
-
             for (int i = array.Length - 1; i >= 0; i--)
             {
                 if (array[i] == 9)
@@ -63,6 +43,11 @@ namespace AddOneProblem
                     array[i] += 1;
                     break;
                 }
+            }
+            if (array[0] == 0)
+            {
+                Array.Resize<int>(ref array, array.Length + 1);
+                array[0] = 1;
             }
         }
     }
